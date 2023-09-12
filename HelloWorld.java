d99d49c09479c1be485df79d7c6317fab24e4450
Miguel Osinada <miguelosinada@gmail.com>
2023-09-12 11:05:24 +0800

    Added Doc Comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,14 @@ class HelloWorld{
     }
 }
 
+/**
+ * @Author: Miguel Osinada
+ * @Version: 1.0
+ * @title: First Java Program
+ * @Description: This program prints "Hello World!" to the console
+ * @date: 12/09/23
+ */
+
 //single line comment
 /* Multi-line Comment */
 /** Java Documentation Comment */
