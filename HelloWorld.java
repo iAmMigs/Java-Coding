6cdb0a018448d9064342736da8313e0da17d7b6d
Miguel Osinada <miguelosinada@gmail.com>
2023-09-12 12:10:13 +0800

    Added comments, New Input Output file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,3 @@
-class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
-
 /**
  * @Author: Miguel Osinada
  * @Version: 1.0
@@ -11,6 +5,29 @@ class HelloWorld{
  * @Description: This program prints "Hello World!" to the console
  * @date: 12/09/23
  */
+
+class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+public class Variables {
+    public static void main(String[] args) {
+        int num1 = 5;
+        byte age;
+
+        String name = "John";
+        String num2 = "19";
+        int inum = Integer.parseInt(num2);
+        Float
+
+    }
+}
+
+// System.out.println(); // prints to console with new line
+// System.out.print(); // prints to console without new line
+// System.out.printf(); // prints formatted string, if output is too long
 
 //single line comment
 /* Multi-line Comment */
---
+++ InputScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InputScanner {
+    public static void main(String[] args){
+        //Instantiate Scanner class
+        Scanner input = new Scanner(System.in);
+        
+        String name = "";
+        int age = 0;
+
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
+        System.out.print("Enter your age: ");
+        age = input.nextInt();
+        System.out.printf("Hello %s, you are %d years old\n", name, age);
+
+        input.close(); //not required, but good practice for closing input screen preventing memory leaks.
+    }
+}
