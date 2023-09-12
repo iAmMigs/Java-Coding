00b806c72a7a2fdb8eed41cc629f9e789d396a4d
Miguel Osinada <miguelosinada@gmail.com>
2023-09-12 10:59:44 +0800

    Added Comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
+
+//single line comment
+/* Multi-line Comment */
