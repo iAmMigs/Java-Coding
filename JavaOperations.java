a3b3917d2a84d9177884db05bfd5dcd307b2ad6d
Miguel Osinada <miguelosinada@gmail.com>
2023-09-19 11:29:56 +0800

    More Operations

--- JavaOperations.java
+++ JavaOperations.java
@@ -2,7 +2,17 @@
 class Operators {
     public static void main(String[] args) {
         int a = 8, b = 2, c = 4, d = 1;
-        System.out.println("c + a / b++ - a * ++d = " + (c + a / b++ - a * ++d));
+        int result = c + a / b++ - a * ++d;
+
+        System.out.println(result);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        System.out.println("Number is " + a + b); // Number is 83
+
+
     }
 }
 
@@ -28,4 +38,25 @@ class Operators {
  * 4 + (8 / 2) - (8 * (++1))
  * 4 + 4 - 16
  * -16
+ * 
+ * Bitwise Operators
+ *      Bitwise AND (&)
+ *      Bitwise OR (|)
+ *      Bitwise XOR (^)
+ *      Bitwise Complement (~)
+ * 
+ * Examples
+ * 8 = 0000 1000
+ * 2 = 0000 0010
+ * 
+ * 2 & 8 = 0000 0000
+ * 
+ * shift operators
+ *     Left shift (<<)
+ *    Right shift (>>)
+ * 
+ * How they work
+ * 8 = 0000 1000
+ * 8 << 2 = 0010 0000
+ * 8 >> 2 = 0000 0010
  */
