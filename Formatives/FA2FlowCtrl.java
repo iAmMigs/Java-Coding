867484235fcded1194aec13a359b7560432badaf
Miguel Osinada <miguelosinada@gmail.com>
2023-10-03 10:17:43 +0800

    FA2

--- Formatives/FA2FlowCtrl.java
+++ Formatives/FA2FlowCtrl.java
@@ -2,24 +2,42 @@ package Formatives;
 
 import java.util.Scanner;
 
-public class FA2FlowCtrl {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int close=0;
-        
-        do{
-            double balance = 0;
-            double interest = 0;
+public class CompoundInterestCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueCalculations = true;
 
-            System.out.print("Enter account balance: ");
-            double balance = input.nextDouble();
-            System.out.print("Enter Interest rate % (ex. 21, 5, 16): ");
-            double interest = input.nextDouble() / 100;
+        while (continueCalculations) {
+            System.out.print("Enter the initial balance: $");
+            double balance = scanner.nextDouble();
+            System.out.print("Enter the annual interest rate (in percentage): ");
+            double annualInterestRate = scanner.nextDouble() / 100;
 
-            
 
-        }while(close != 1);
+            double futureValueAnnually = calculateFutureValue(balance, annualInterestRate, 1);
+            double futureValueMonthly = calculateFutureValue(balance, annualInterestRate / 12, 12);
+            double futureValueDaily = calculateFutureValue(balance, annualInterestRate / 365, 365);
 
-        input.close();
+            // Display results
+            System.out.println("\nFuture Value with Annual Compounding: $" + futureValueAnnually);
+            System.out.println("Future Value with Monthly Compounding: $" + futureValueMonthly);
+            System.out.println("Future Value with Daily Compounding: $" + futureValueDaily);
+
+            // Ask if the user wants to continue
+            System.out.print("\nDo you want to repeat the program? (yes/no): ");
+            String input = scanner.next();
+            if (!input.equalsIgnoreCase("yes")) {
+                continueCalculations = false;
+            }
+        }
+
+        System.out.println("Program ended.");
+    }
+
+    public static double calculateFutureValue(double balance, double monthlyInterestRate, int periods) {
+        for (int i = 0; i < periods; i++) {
+            balance += balance * monthlyInterestRate;
+        }
+        return balance;
     }
 }
