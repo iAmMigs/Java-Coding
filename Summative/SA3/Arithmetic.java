fa3c2c42f7e6fab9140e88d6923177866f997df8
Miguel Osinada <miguelosinada@gmail.com>
2023-11-07 09:14:00 +0800

    Added Arithmetic

---
+++ Summative/SA3/Arithmetic.java
@@ -0,0 +1,33 @@
+package Summative.SA3;
+
+public class Arithmetic {
+ public int add(int n1, int n2){
+     return n1 + n2;
+ }
+
+ public int add(int n1, int n2, int n3){
+     return n1 + n2 + n3;
+ }
+
+ public int add(int n1, int n2, int n3, int n4){
+     return n1 + n2 + n3 + n4;
+ }
+
+ public int subtract(int n1, int n2){
+     return n1 - n2;
+ }
+
+ public int multiply(int n1, int n2){
+     return n1 * n2;
+ }
+
+ public int multiply(int n1, int n2, int n3){
+     return n1 * n2 * n3;
+ }
+
+ public int divide(int n1, int n2){
+     return n1 / n2;
+ }
+
+
+}
